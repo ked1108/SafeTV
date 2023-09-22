8b261f0c01be6e6e37be095179e8bd74265b7b2d
negiisha <negiisha824@gmail.com>
2023-09-23 01:19:45 +0530

    frontend done | 🙀

--- app.py
+++ app.py
@@ -24,11 +24,16 @@ def video_feed():
 
 @app.route('/')
 def index():
-    return render_template("about.html")
+    return render_template("index.html")
 
 @app.route('/contact')
 def contact():
     return render_template("contacts.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
